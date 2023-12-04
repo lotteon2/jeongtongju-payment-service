948cb82e04fd56ca2631b55f452c0d765ff41842
최성훈 <33412452+realsuperman@users.noreply.github.com>
2023-12-04 16:32:49 +0900

    FEAT - Order에서 사용하는 결제 Feign 추가 - FeignFormat 수정 SCRUM-165

---
+++ src/main/java/com/jeontongju/payment/controller/PaymentFeignController.java
@@ -0,0 +1,40 @@
+package com.jeontongju.payment.controller;
+
+import com.jeontongju.payment.dto.temp.FeignFormat;
+import com.jeontongju.payment.dto.temp.KakaoPayApproveDto;
+import com.jeontongju.payment.dto.temp.KakaoPayMethod;
+import com.jeontongju.payment.dto.temp.PaymentMethod;
+import com.jeontongju.payment.enums.temp.PaymentMethodEnum;
+import com.jeontongju.payment.util.KakaoPayUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class PaymentFeignController {
+    private final KakaoPayUtil kakaoPayUtil;
+
+    @PostMapping("/{paymentMethod}/pay-approve")
+    public FeignFormat<Void> approveKakaopay(@PathVariable("paymentMethod") PaymentMethodEnum paymentMethodEnum, @RequestBody PaymentMethod paymentMethod) {
+        if(paymentMethodEnum == PaymentMethodEnum.KAKAO){
+            KakaoPayMethod kakaoPayMethod = (KakaoPayMethod) paymentMethod;
+            kakaoPayUtil.callKakaoApproveApi(KakaoPayApproveDto.builder()
+                            .tid(kakaoPayMethod.getTid())
+                            .pgToken(kakaoPayMethod.getPgToken())
+                            .partnerOrderId(kakaoPayMethod.getPartnerOrderId())
+                            .partnerUserId(kakaoPayMethod.getPartnerUserId())
+                    .build());
+        }
+        return FeignFormat.<Void>builder()
+                .code(HttpStatus.SC_OK)
+                .build();
+    }
+}
